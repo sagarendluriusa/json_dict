4e5bdd965f333f265d276460d58c7eebffe5541f
sagarendluriusa <sagarendluri.usa@gmail.com>
2025-04-27 17:28:26 -0400

    Update convert_json_to_dict.py

--- convert_json_to_dict.py
+++ convert_json_to_dict.py
@@ -1,13 +1,17 @@
-from langchain_community.chat_models import ChatOllama
-from langchain.prompts import ChatPromptTemplate
+import streamlit as st
+import json
 
-llm = ChatOllama(model="llama3.2:latest", temperature=0)
-negotiate_prompt = ChatPromptTemplate.from_messages([
-        ("system", "You are a well python programmer and senior developer, convert user json input format into dict format "),
-    
-        ("user", "{question}")
-    ])
-query = {"name":"John", "age":30, "car":"null"}
-formatted_prompt = negotiate_prompt.format(question=query)
-response = llm.invoke(formatted_prompt )
-print(response)
+# Show the form
+st.title('Submit Form')
+
+option = st.radio('Select an option', ('Json', 'Dictionary'))
+user_input = st.text_input('Enter your data')
+
+if st.button('Submit'):
+    # Process the data and store or handle it
+    response = {
+        'option': option,
+        'userInput': user_input
+    }
+    st.write('Form Submitted Successfully!')
+    st.json(response)
